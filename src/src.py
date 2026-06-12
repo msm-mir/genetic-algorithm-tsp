999c8c6b077d81ea5e83c97edc306fab7b848ac0
marble <msm.mirdamad@gmail.com>
2026-06-12 21:59:56 +0330

    add distance_of_cities function to calculate distance matrix for city pairs

--- src/src.py
+++ src/src.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 # generate coordinate (x, y) of n cities
 def generate_cities(n):
@@ -12,6 +13,22 @@ def generate_cities(n):
     
     return list(cities_set)
 
+# calculate distance matrix for all pairs of cities
+def distance_of_cities(cities):
+    # euclidean distance
+    def dis(c1, c2):
+        x1, y1 = c1
+        x2, y2 = c2
+        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+    # n*n distance matrix
+    distance_matrix = [[dis(c1, c2) for c2 in cities] for c1 in cities]
+    return distance_matrix
+
+
 # generate inputs (coordinate of the cities)
 n_cities = 100
 cities = generate_cities(n_cities)
+
+# euclidean distance for city pairs
+distance_matrix = distance_of_cities(cities)
